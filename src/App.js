5e97bd6623c9748db7bbc91e0ae105dcab6382a1
Ben <benthompson4496@gmail.com>
2018-06-24 18:50:02 -0500

    pushing for help in Slack

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+//instructions say to 'copy contents of script tag' - but there are multiple...is that a problem?
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAZD-XBeBUsyE0ijFKRMK_itLV6mFi4GfI",
+  authDomain: "bloc-chat-c9fa9.firebaseapp.com",
+  databaseURL: "https://bloc-chat-c9fa9.firebaseio.com",
+  projectId: "bloc-chat-c9fa9",
+  storageBucket: "bloc-chat-c9fa9.appspot.com",
+  messagingSenderId: "408308171709"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
---
+++ src/components/RoomList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+
+
+class RoomList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rooms: []
+        };
+        this.roomsRef = this.props.firebase.database().ref('rooms');
+    }
+
+    componentDidMount() {
+        this.roomsRef.on('child-added', snapshot => {
+            const room = snapshot.val();
+            room.key = snapshot.key;
+            this.setState({ room: this.state.rooms.concat( room ) })
+        });
+    }
+
+
+
+
+    render() {
+        return(
+            <section className="room-list">
+                {
+                    this.state.rooms.map( (room, key) =>
+                        <div className="room-data" key={room.key}>
+                           {room} 
+                        </div>
+                    )
+                }
+            </section>
+        );
+    }
+}
+
+export default RoomList;
